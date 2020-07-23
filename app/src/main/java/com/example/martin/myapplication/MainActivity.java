3b6596ae45dc64c15629f29b2db66c8e1de934b4
Martin Ruiz (Linux Mint) <arcollector@gmail.com>
2020-07-23 13:24:49 -0300

    added hello_world_salute textview

--- app/src/main/java/com/example/martin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/martin/myapplication/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.martin.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.Observable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @BindView(R.id.hello_world_salute)
+    TextView helloText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        Observable.just("Please use this app responsibly!")
+                .subscribe(s -> helloText.setText(s));
+
+        /*
+        Observable.just(
+                new StockUpdate("GOOGLE", 12.43, new Date()),
+                new StockUpdate("APPL", 645.1, new Date()),
+                new StockUpdate("TWTR", 1.43, new Date())
+        ).subscribe(stockUpdate -> {
+            Log.d(TAG, "New update " + stockUpdate.getStockSymbol());
+            stockDataAdapter.add(stockUpdate);
+        });
+        */
     }
 }
